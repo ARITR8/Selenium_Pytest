2026210ea1e591d1dae232c1f3ca1669be281ac5
Ari <aritradas75@gmail.com>
2025-02-03 23:51:16 +0530

    Changes for selenium pytest project

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -27,6 +27,7 @@ def pytest_configure(config):
     config._metadata['Tester'] = 'Pavan'
 
 # It is hook for delete/Modify Environment info to HTML Report
+##push
 @pytest.mark.optionalhook
 def pytest_metadata(metadata):
     metadata.pop("JAVA_HOME", None)
